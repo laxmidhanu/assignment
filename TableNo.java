8f21815d1eb5652c7f0379406e21f3048103961b
laxmidhanu <127470276+laxmidhanu@users.noreply.github.com>
2023-03-24 17:07:14 +0530

    Add files via upload

---
+++ TableNo.java
@@ -0,0 +1,9 @@
+class TableNo {
+	public static  void main(String[] args) {
+		int no=7;
+		System.out.println("Table of 7 :\n");
+		for(int i=1; i<=10; i++){
+		System.out.println(no*i);
+		}
+	}
+}
