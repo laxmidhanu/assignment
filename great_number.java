343d3ee95e9a18fa36be3dec0668f593a0e37807
laxmidhanu <127470276+laxmidhanu@users.noreply.github.com>
2023-03-24 17:00:00 +0530

    Add files via upload

---
+++ great_number.java
@@ -0,0 +1,17 @@
+class great_number {
+	public static  void main(String[] args) {
+		int i=99;
+		int n=46;
+		int m=87;
+		if (i>n && i>m){
+		System.out.println(i + " is Greatest Number");
+		}
+		else if (i<n && n>m){
+		System.out.println(n + " is Greatest Number");
+		}
+		else {
+			System.out.println(m + " is Greatest Number");
+		
+		}
+	}
+}
