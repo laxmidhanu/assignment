31a1f848351ac7b6ea4b9e9839cebf83857f336d
laxmidhanu <127470276+laxmidhanu@users.noreply.github.com>
2023-03-24 17:01:59 +0530

    Add files via upload

---
+++ Leap_Year.java
@@ -0,0 +1,11 @@
+class Leap_Year {
+	public static  void main(String[] args) {
+		int year=2023;
+		if (year%4==0){
+		System.out.println(year + " is Leap Year");
+		}
+		else
+			System.out.println(year + " is Not Leap Year");
+		
+		}
+		}
