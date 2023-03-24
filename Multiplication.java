bfad12837529e976359b96d5eda93bff55726d00
laxmidhanu <127470276+laxmidhanu@users.noreply.github.com>
2023-03-24 23:10:15 +0530

    Add files via upload

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+class Multiplication{
+
+public static void main( String args[]){
+ int a = 9;
+ int b = 3;
+
+System.out.println(a + " " + b);
+int multi = a * b;
+System.out.println("Multiplication = " + multi);
+
+}
+
+}
