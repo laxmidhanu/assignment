6e3af1998cf1bbf705db9cee66c51297866e9e03
laxmidhanu <127470276+laxmidhanu@users.noreply.github.com>
2023-03-24 16:50:59 +0530

    Add files via upload

---
+++ no_is_negative.java
@@ -0,0 +1,11 @@
+class no_is_negative {
+	public static  void main(String[] args) {
+		int i=-10;
+		if (i<0){
+		System.out.println(i + " is Negative Number");
+		}
+		else
+			System.out.println(i + " is Positive Number");
+		
+		}
+		}
